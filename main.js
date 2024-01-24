4039463f0f85f69758774fb4a657cad6a51166d8
Habiba Anika <anikahabiba49@gmail.com>
2024-01-24 11:18:08 +0600

    help desk area half added

---
+++ main.js
@@ -0,0 +1,11 @@
+let menu = document.getElementById("menu")
+menu.style.maxHeight = "0px";
+
+function toggle() {
+    if (menu.style.maxHeight == "0px") {
+        menu.style.maxHeight = "400px";
+    }
+    else{
+        menu.style.maxHeight = "0px"; 
+    }
+}
